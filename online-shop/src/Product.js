dc3373007ffe134e5b5591c29b862bf4a6346579
IonutCosmina <68537300+IonutCosmina@users.noreply.github.com>
2020-07-23 18:41:47 +0300

    hooks

--- online-shop/src/Product.js
+++ online-shop/src/Product.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
 function Product(props) {
+
+    //hooks
+   const[quantity, setQuantity]=useState(0);
+   const handleClick = () => setQuantity(quantity + 1)
+   const handleClick2 = () => {if (quantity>0){setQuantity(quantity-1)}}
    
+  
+
+
     return (<div className="ProductComponent">
         <div className="ProductDetails">
             <div className="ProductName">{props.name}</div>
             <div className="ProductCategory">{props.category}</div>
             <div className="ProductPrice"> ${props.price}</div>
+            
+            <div id="quantity">Quantity: {quantity}
+                <button className="QuantityButton" onClick={handleClick}>+</button>
+                <button className="QuantityButton" onClick={handleClick2}>-</button>
+            </div>
         </div>
-        <div className="ProductDescription"> Description {props.description}</div>
+        <div className="ProductDescription">{props.description}</div>
 
     </div>);
 
